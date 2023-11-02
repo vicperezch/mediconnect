b21b88a467c4bd6a602ca81e884227b27bf0bd58
Diego Flores <floresdiego041@gmail.com>
2023-11-02 16:17:25 -0600

    Agregando la vista agregar citas

---
+++ src/com/mediconnect/view/CitasMedicoGUI.java
@@ -0,0 +1,23 @@
+package com.mediconnect.view;
+
+import javax.swing.*;
+
+public class CitasMedicoGUI {
+    private JPanel pnlCitasMedico;
+    private JPanel pnlHeader;
+    private JLabel lblTitle;
+    private JLabel lblSubtitle;
+    private JPanel pnlBody;
+    private JPanel pnlForm;
+    private JPanel pnlVerCitas;
+    private JTable tblCitas;
+    private JLabel lblTituloTabla;
+    private JLabel lblAgregaCitas;
+    private JPanel pnlAgregarCitas;
+    private JTextField txtEstablecimiento;
+    private JTextField txtFecha;
+    private JTextField txtHora;
+    private JButton btnRegresar;
+    private JComboBox cmbPaciente;
+    private JButton btnAgregarCita;
+}
