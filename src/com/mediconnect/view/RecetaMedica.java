a2e48723fb220bef6ed74fbc5d36dfe4778596b4
nilsmuralles <murallesnils11@gmail.com>
2023-11-02 15:45:08 -0600

    Vista de receta médica

---
+++ src/com/mediconnect/view/RecetaMedica.java
@@ -0,0 +1,19 @@
+package com.mediconnect.view;
+
+import javax.swing.*;
+
+public class RecetaMedica {
+    private JPanel pnlRecetaMedica;
+    private JPanel pnlHeader;
+    private JLabel lblTitle;
+    private JLabel lblSubtitle;
+    private JPanel pnlBody;
+    private JPanel pnlForm;
+    private JComboBox cmbPaciente;
+    private JTextField txtMedicamentos;
+    private JButton btnMedicamentos;
+    private JTextField txtJustificacion;
+    private JTextField txtObservaciones;
+    private JLabel lblAgregarRecetas;
+    private JTable tblRecetas;
+}
