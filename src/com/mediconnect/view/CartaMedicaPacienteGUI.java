d6e6c6bb9222786c7e1d635c9db6144d354e6995
Diego Flores <floresdiego041@gmail.com>
2023-11-02 18:42:17 -0600

    Agregando la vista de carta medica del paciente

---
+++ src/com/mediconnect/view/CartaMedicaPacienteGUI.java
@@ -0,0 +1,19 @@
+package com.mediconnect.view;
+
+import javax.swing.*;
+
+public class CartaMedicaPacienteGUI {
+    private JPanel pnlCartaMedicaPaciente;
+    private JPanel pnlHeader;
+    private JLabel lblTitle;
+    private JLabel lblSubTitle;
+    private JPanel pnlBody;
+    private JPanel pnlForm;
+    private JButton btnRegresar;
+    private JPanel pnlEnfermedades;
+    private JPanel pnlAlergias;
+    private JLabel pnlExamenes;
+    private JTable tblEnfermedades;
+    private JTable tblAlergias;
+    private JTable tblExamenes;
+}
