93e2d66fd1e2f3cb41338ae997d6e805f526740a
Diego Flores <floresdiego041@gmail.com>
2023-11-02 21:12:10 -0600

    Agregando la vista para ver recetas del paciente

---
+++ src/com/mediconnect/view/RecetasMedicasPaciente.java
@@ -0,0 +1,14 @@
+package com.mediconnect.view;
+
+import javax.swing.*;
+
+public class RecetasMedicasPaciente {
+    private JPanel pnlRecetasMedicasPaciente;
+    private JPanel pnlHeader;
+    private JLabel lblTitle;
+    private JLabel lblSubtitle;
+    private JPanel pnlBody;
+    private JPanel pnlForm;
+    private JTable tblRecetas;
+    private JButton btnRegresar;
+}
