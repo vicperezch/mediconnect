8c107053a7e937099858d5ade40b7c749bd2f7d2
Diego Flores <floresdiego041@gmail.com>
2023-11-02 22:19:16 -0600

    Agregando a vista para ver las citas del paciente

---
+++ src/com/mediconnect/view/CitasPacienteGUI.java
@@ -0,0 +1,15 @@
+package com.mediconnect.view;
+
+import javax.swing.*;
+
+public class CitasPacienteGUI {
+    private JPanel pnlCitasPaciente;
+    private JPanel pnlHeader;
+    private JLabel lblTitle;
+    private JLabel lblSubtitle;
+    private JPanel pnlBody;
+    private JButton btnRegresar;
+    private JTable tblCitas;
+    private JPanel pnlForm;
+
+}
